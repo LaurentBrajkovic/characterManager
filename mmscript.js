17ae8ba0f132a11eb0d6846fc08309e2f1338d44
Lucas Ielli <lucas.ielli.pro@gmail.com>
2021-08-11 17:05:51 +0200

    Some try on the JS

--- mmscript.js
+++ mmscript.js
@@ -1,27 +1,47 @@
-// document.getElementById('charName').innerHTML = ''
+// (async () => {
 
-// document.getElementById('charView').addEventListener('click', ()=>{});
+//     let listOfChar = await fetch('https://character-database.becode.xyz/characters');
+//     console.log(listOfChar);
+//     let convertChar = await listOfChar.json();
+//     console.log(convertChar);
+//     let target = document.getElementById('target');
+//     console.log(target);
 
-(async () => {
+//     convertChar.forEach(character => {
+        
+//     });
+// })();
 
-    let listOfChar = await fetch('https://character-database.becode.xyz/characters');
-    let transformText = await listOfChar.json();
-    console.log(transformText);
-    // let template = document.getElementsById('tpl-char');
-    // let target = document.getElementsById('target');
+// Fetching database
+(async()=>{
+    let template = document.getElementById('tpl-char');
+    let target = document.getElementById('target');
+    console.log(target);
+    let listOfChar = await fetch('https://character-database.becode.xyz/characters')
+    let convertChar = await listOfChar.json()
 
+    
+    convertChar.forEach(character => {
+        let clone = template.content.cloneNode(true);
+        // const documentImage = clone.getElementById('charImg');
+        let documentName = clone.getElementById('charName');
+        let documentShortDescr = clone.getElementById('charShortDescr');
+        
 
-
-    TransformText.forEach(character => {
-        // let clone = template.content.cloneNode(true);
-        let image = document.querySelector('.img');
-        let name = document.querySelector('.CharName');
-        let shortDescription = document.querySelector('.charShortDescr');
-
-        image.innerHTML = character.image;
-        name.innerHTML = character.name;
-        shortDescription.innerHTML = character.shortDescription;
+        documentName.innerHTML = character.name;
+        documentShortDescr.innerHTML = character.shortDescription;
+        // documentImage.setAttribute('src', `convertChar:image/jpeg;base64,${character.image}`);
         target.appendChild(clone);
-
+        target.innerHTML = 'Coucou'
     });
 })();
+
+// Add a new character to the collection
+document.getElementById('charAdd').addEventListener('click',()=>{
+ console.log('I wanna add'); // Open character creator/editor
+});
+// Search character
+document.getElementById('charSearch').addEventListener('input',()=>{
+    let inputCharSearch = document.getElementById('charSearch').value;
+    console.log(inputCharSearch);
+});
