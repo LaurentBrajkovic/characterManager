baf1746d3b8e3c4845ab5e51c007cf8cdbd9abc8
Laurent <laurent.brajkovic1@gmail.com>
2021-08-11 14:05:23 +0200

    Update on CSS & Bit of JS

--- mmscript.js
+++ mmscript.js
@@ -1,3 +1,9 @@
 // document.getElementById('charName').innerHTML = ''
 
 // document.getElementById('charView').addEventListener('click', ()=>{});
+
+(() => {
+
+    let listOfChar = await fetch('')
+
+})();
