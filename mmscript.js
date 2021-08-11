dc00c97d02ddacd3a81db79f56b184e7113f7515
Laurent <laurent.brajkovic1@gmail.com>
2021-08-11 15:02:50 +0200

    Added JS attempt one

--- mmscript.js
+++ mmscript.js
@@ -2,8 +2,26 @@
 
 // document.getElementById('charView').addEventListener('click', ()=>{});
 
-(() => {
+(async () => {
 
-    let listOfChar = await fetch('')
+    let listOfChar = await fetch('https://character-database.becode.xyz/characters');
+    let transformText = await listOfChar.json();
+    console.log(transformText);
+    // let template = document.getElementsById('tpl-char');
+    // let target = document.getElementsById('target');
 
+
+
+    TransformText.forEach(character => {
+        // let clone = template.content.cloneNode(true);
+        let image = document.querySelector('.img');
+        let name = document.querySelector('.CharName');
+        let shortDescription = document.querySelector('.charShortDescr');
+
+        image.innerHTML = character.image;
+        name.innerHTML = character.name;
+        shortDescription.innerHTML = character.shortDescription;
+        target.appendChild(clone);
+
+    });
 })();
